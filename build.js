2b7d5369cc0fa975843c18f626a028e618971270
Jon Deaves <jon.deaves@fanduel.com>
2020-04-04 14:00:37 +0100

    chore: change my role

--- build.js
+++ build.js
@@ -1,57 +1,68 @@
-'use strict'
+"use strict";
 
 // Pull in our modules
-const chalk = require('chalk')
-const boxen = require('boxen')
-const fs = require('fs')
-const path = require('path')
+const chalk = require("chalk");
+const boxen = require("boxen");
+const fs = require("fs");
+const path = require("path");
 
 // Define options for Boxen
 const options = {
   padding: 1,
   margin: 1,
-  borderStyle: 'round'
-}
+  borderStyle: "round",
+};
 
-const username = 'jondeaves'
+const username = "jondeaves";
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('               Jon Deaves'),
+  name: chalk.white("               Jon Deaves"),
   handle: chalk.white(username),
-  work: chalk.white(`Software Developer at ${chalk.blue('FanDuel')}`),
-  npm: chalk.gray('https://npmjs.com/') + chalk.green(`~${username}`),
-  github: chalk.gray('https://github.com/') + chalk.green(username),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.green(username),
-  web: chalk.cyan('https://jondeaves.me/'),
+  work: chalk.white(`Senior Software Developer at ${chalk.blue("FanDuel")}`),
+  npm: chalk.gray("https://npmjs.com/") + chalk.green(`~${username}`),
+  github: chalk.gray("https://github.com/") + chalk.green(username),
+  linkedin: chalk.gray("https://linkedin.com/in/") + chalk.green(username),
+  web: chalk.cyan("https://jondeaves.me/"),
   npx: chalk.white(`npx ${username}`),
 
-  labelWork: chalk.white.bold('       Work:'),
-  labelnpm: chalk.white.bold('        npm:'),
-  labelGitHub: chalk.white.bold('     GitHub:'),
-  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
-  labelWeb: chalk.white.bold('        Web:'),
-  labelCard: chalk.white.bold('       Card:')
-}
+  labelWork: chalk.white.bold("       Work:"),
+  labelnpm: chalk.white.bold("        npm:"),
+  labelGitHub: chalk.white.bold("     GitHub:"),
+  labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+  labelWeb: chalk.white.bold("        Web:"),
+  labelCard: chalk.white.bold("       Card:"),
+};
 
 // Actual strings we're going to output
-const newline = '\n'
-const heading = `${data.name} / ${data.handle}`
-const working = `${data.labelWork}  ${data.work}`
-const npming = `${data.labelnpm}  ${data.npm}`
-const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
-const carding = `${data.labelCard}  ${data.npx}`
+const newline = "\n";
+const heading = `${data.name} / ${data.handle}`;
+const working = `${data.labelWork}  ${data.work}`;
+const npming = `${data.labelnpm}  ${data.npm}`;
+const githubing = `${data.labelGitHub}  ${data.github}`;
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`;
+const webing = `${data.labelWeb}  ${data.web}`;
+const carding = `${data.labelCard}  ${data.npx}`;
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + // data.name + data.handle
-               newline + newline + // Add one whole blank line
-               working + newline + // data.labelWork + data.work
-               npming + newline + // data.labelnpm + data.npm
-               githubing + newline + // data.labelGitHub + data.github
-               linkedining + newline + // data.labelLinkedIn + data.linkedin
-               webing + newline + newline + // data.labelWeb + data.web
-               carding // data.labelCard + data.npx
+const output =
+  heading + // data.name + data.handle
+  newline +
+  newline + // Add one whole blank line
+  working +
+  newline + // data.labelWork + data.work
+  npming +
+  newline + // data.labelnpm + data.npm
+  githubing +
+  newline + // data.labelGitHub + data.github
+  linkedining +
+  newline + // data.labelLinkedIn + data.linkedin
+  webing +
+  newline +
+  newline + // data.labelWeb + data.web
+  carding; // data.labelCard + data.npx
 
-fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
+fs.writeFileSync(
+  path.join(__dirname, "bin/output"),
+  chalk.green(boxen(output, options))
+);
